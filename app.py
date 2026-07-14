959e95953505a1067ac0b3c7f33d61a0c75c676f
jericayuan <jxyuan@uci.edu>
2026-07-13 22:26:14 -0700

    feat: add annual_filing function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 class SecEdgar:
     def __init__(self, fileurl):
@@ -6,8 +7,8 @@ class SecEdgar:
         self.namedict = {}
         self.tickerdict = {}
 
-        headers = {'user-agent': 'jxyuan@uci.edu'}
-        r = requests.get(self.fileurl, headers=headers)
+        self.headers = {'user-agent': 'jxyuan@uci.edu'}
+        r = requests.get(self.fileurl, headers=self.headers)
         
         self.filejson = r.json()
 
@@ -31,6 +32,31 @@ class SecEdgar:
     def ticker_to_cik(self, ticker):
         return self.tickerdict.get(ticker.lower())
 
+    def annual_filing(self, cik, year):
+        response = requests.get(f"https://data.sec.gov/submissions/CIK{cik}.json", headers=self.headers)
+        data = response.json()
+        
+        dates = data['filings']['recent']['filingDate']
+        form = data['filings']['recent']['form']
+        for i, date in enumerate(dates):
+            if form[i] == '10-K':
+                filing_date_year = dates[i].split('-')[0]
+                if str(year) == filing_date_year:
+                    accessionNumber = data['filings']['recent']['accessionNumber'][i]
+                    primaryDocument = data['filings']['recent']['primaryDocument'][i]
+
+                    return {
+                        "date": date,
+                        "accessionNumber": accessionNumber.replace("-", ""),
+                        "primaryDocument": primaryDocument,
+                        "cik": cik
+                    }
+        print(f"No 10-K filing found for {year}")
+        return {}
+    
+    
+
 se = SecEdgar('https://www.sec.gov/files/company_tickers.json')
-print(se.ticker_to_cik('AAPL'))
+print(se.annual_filing('0000320193', 2020))
+print(se.quarterly_filing('0000320193', 2020, "Q2"))
 print(se.name_to_cik('Apple Inc.'))
